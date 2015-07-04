27cd7e42330314d94f61a21a1dcc26930f33bb99
ayushajain <ayushajain@gmail.com>
2015-07-03 22:59:54 -0700

    added aldio main github

---
+++ script.js
@@ -0,0 +1,80 @@
+
+var ref = new Firebase('https://hiveio.firebaseio.com/');
+
+var user = replacePeriods('rohan@techlabeducation.com')
+
+var portAvailability = [] //true means available , false means not available
+
+getPortAvailability()
+//createUser('rohan@techlabeducation.com', 'jenga') //this should be called from html when a button is pressed or something
+
+function createUser(email, password){
+    var newEmail = replacePeriods(email)
+    ref.createUser({
+        email    : email,
+        password : password
+    }, function(error, userData) {
+        if (error) {
+            console.log("Error creating user:", error);
+        } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+            ref.child('users').child(newEmail).set({
+                ports: {
+                    port1: {
+                        type: 'empty',
+                        value: 'empty',
+                    },
+                    port2: {
+                        type: 'empty',
+                        value: 'empty'
+                    },
+                    port3: {
+                        type: 'empty',
+                        value: 'empty'
+                    },
+                    port4: {
+                        type: 'empty',
+                        value: 'empty'
+                    }
+                } ,
+                settings: {
+                    email: email
+                }
+            })
+        }
+    });
+}
+
+function changeAvailability(num){
+    var value = ''
+    if(portAvailability[num-1] == 'empty'){
+        value = 'please enter a sensor type'
+    }else{
+        value = 'empty'
+    }
+
+    ref.child('users').child(user).child('ports').child('port'+ num).update({
+        type: value
+    })
+
+    getPortAvailability()
+}
+
+function getPortAvailability(){
+
+    ref.child('users').child(user).child('ports').on("value", function(snapshot) {
+        var index = 0
+        snapshot.forEach(function(childSnapshot){
+            //console.log(childSnapshot.child('type').val())
+            portAvailability[index] = childSnapshot.child('type').val()
+
+
+            index++
+        })
+    });  
+
+}
+
+function replacePeriods(email){
+    return email.replace(/\./g,'*')
+}
